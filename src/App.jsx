1a3343c06f57744e587f397ee0f8cc81b3e76812
NikitMehtaTU <nikit.762419@thc.tu.edu.np>
2025-05-26 18:44:54 +0545

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,22 @@ function App() {
   return (
     <>
       <p>Hi</p>
+
+      <h2>Chapter 2</h2>
+      <iframe
+        src="/chapter-2.pdf"
+        width="100%"
+        height="600px"
+        title="Chapter 2"
+      ></iframe>
+
+      <h2>Chapter 3</h2>
+      <iframe
+        src="/chapter-3.pdf"
+        width="100%"
+        height="600px"
+        title="Chapter 3"
+      ></iframe>
     </>
   );
 }
